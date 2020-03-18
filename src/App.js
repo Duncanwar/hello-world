137449dfe3fb73652ae2d37ea7cef46f634f6e55
Duncan <semunda9@gmail.com>
2020-03-18 21:30:02 +0200

    Intro to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Greet from '.components/Greet'
+import Welcome from '.components/Welcome'
+import Hello from '.components/Hello'
 
-function App() {
+class App extends Component{
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hello world</h1>
       </header>
+      <Greet/>
+      <Welcome/>
+      <Hello/>
     </div>
   );
 }
 
+}
 export default App;
